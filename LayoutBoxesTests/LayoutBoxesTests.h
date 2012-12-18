2ca165de994ac75722e4bdba1b8964fdc156fd3f
Daniel Monego <dan@Daniel-Monegos-MacBook-Pro.local>
2012-12-18 08:06:22 -0500

    Added test project.

---
+++ LayoutBoxesTests/LayoutBoxesTests.h
@@ -0,0 +1,17 @@
+//
+//  LayoutBoxesTests.h
+//  LayoutBoxesTests
+//
+//  Created by Daniel Monego on 12/17/12.
+//  Copyright 2012 CDM. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface LayoutBoxesTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ LayoutBoxesTests/LayoutBoxesTests.m
@@ -0,0 +1,33 @@
+//
+//  LayoutBoxesTests.m
+//  LayoutBoxesTests
+//
+//  Created by Daniel Monego on 12/17/12.
+//  Copyright 2012 CDM. All rights reserved.
+//
+
+#import "LayoutBoxesTests.h"
+
+
+@implementation LayoutBoxesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LayoutBoxesTests");
+}
+
+@end
